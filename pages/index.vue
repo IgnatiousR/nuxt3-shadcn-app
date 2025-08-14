0180e9938cfb6c2a2e4b7ecd85b7a9b25a370760
Ignatious Rozario <ignatiousr80@gmail.com>
2025-08-14 17:31:23 +0600

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -28,6 +28,9 @@ const formSchema = toTypedSchema(
     email: z.string().email().min(2).max(50),
     password: z.string().min(6),
     password_confirmation: z.string().min(6),
+  }).refine((data) => data.password === data.password_confirmation, {
+    message: "Passwords don't match",
+    path: ['password_confirmation'],
   })
 );
 
