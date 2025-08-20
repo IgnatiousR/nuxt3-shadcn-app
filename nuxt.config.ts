3f1d174f4c6f75fbefcfa85c00b787bdd1c9493c
Ignatious Rozario <ignatiousr80@gmail.com>
2025-08-20 12:42:15 +0600

    Init

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,35 +1,27 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 import tailwindcss from "@tailwindcss/vite";
 export default defineNuxtConfig({
-  runtimeConfig: {
-    databaseUrl: process.env.DATABASE_URL,
-    jwtSecret: process.env.JWT_SECRET,
-  },
-  compatibilityDate: "2025-07-15",
+  compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  modules: [
-    "@nuxt/eslint",
-    "@nuxt/image",
-    "shadcn-nuxt",
-    "@formkit/auto-animate/nuxt",
-    "@nuxtjs/color-mode",
-  ],
-  css: ["~/assets/css/main.css"],
+  modules: ['@nuxt/eslint', '@nuxt/image', 'shadcn-nuxt',  '@nuxtjs/color-mode'],
+  css: ['~/assets/css/main.css'],
   vite: {
-    plugins: [tailwindcss()],
+    plugins: [
+      tailwindcss(),
+    ],
+  },
+  colorMode: {
+    classSuffix: ''
   },
   shadcn: {
     /**
      * Prefix for all the imported component
      */
-    prefix: "",
+    prefix: '',
     /**
      * Directory that the component lives in.
      * @default "./components/ui"
      */
-    componentDir: "./components/ui",
+    componentDir: './components/ui'
   },
-  colorMode: {
-    classSuffix: "",
-  },
-});
+})
